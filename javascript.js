f3bc14c373ac4542da564f7a9007fdf4bee5bd65
Jason Collins <the.oem@gmail.com>
2017-03-22 23:45:09 -0600

    Phase 3 requirements completed. - Add additional inputs that allow the user to specify the minimum/maximum range. - Upon successful win, user’s range is updated:    Every time the user wins a round increase the maximum number by 10.    Every time the user wins a round decrease the minimum number by 10.    Appropriate UI is incorporated such that user understands what is happening.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Set the global variables that will be needed.
 var guessInput = document.getElementById('guessInput');
 var guessButton = document.getElementById('guessButton');
 var clearButton = document.getElementById('clearButton');
@@ -6,35 +7,62 @@ var guessMessageTop = document.getElementById('guessMessageTop');
 var guessValue = document.getElementById('guessValue');
 var guessMessageBottom = document.getElementById('guessMessageBottom');
 var errorMessages = document.getElementById('errorMessages');
+var minRangeValue = document.getElementById('minRangeValue');
+var maxRangeValue = document.getElementById('maxRangeValue');
 var randomNumber = null;
 var usersLastGuess = null;
 
-// When the user clicks the CLEAR button, reset the input field state
+// Event listener on the CLEAR button. We need to clear the input element and disable the appropriate buttons.
 clearButton.addEventListener('click', function () {
-	console.log('CLEAR clicked.');
 	clearInput();
-	disableButton(clearButton);
+	disableElement(clearButton);
 });
 
+// Event listener on the RESET button. We need to clear the input element and reset back to zero state.
 resetButton.addEventListener('click', function () {
-	console.log('RESET clicked.');
+	console.log('=+=+=+=+=+=+ NEW GAME =+=+=+=+=+=+');
 	clearInput();
 	setZeroState();
 });
 
+// Event listener that checks the input of the Min Range to ensure it is a valid number.
+minRangeValue.addEventListener('input', function () {
+	var minVal = parseInt(minRangeValue.value, 10);
+	if (isNaN(minVal)) {
+		errorMessages.innerText = "Min range value must be a valid number.";
+	} else {
+		minRangeValue.value = minVal;
+	}
+})
+
+// Event listener that checks the input of the Max Range to ensure it is a valid number.
+maxRangeValue.addEventListener('input', function () {
+	var maxVal = parseInt(maxRangeValue.value, 10);
+	if (isNaN(maxVal)) {
+		errorMessages.innerText = "Max range value must be a valid number.";
+	} else {
+		maxRangeValue.value = maxVal;
+	}
+})
+
 // User clicks guess, so we need to evaluate it against the random number
 guessButton.addEventListener('click', function () {
-	console.log('GUESS clicked.');
-	enableButton(resetButton);
+	enableElement(resetButton);
 	usersLastGuess = parseInt(guessInput.value, 10);
+	minVal = parseInt(minRangeValue.value, 10);
+	maxVal = parseInt(maxRangeValue.value, 10);
 	if (isNaN(usersLastGuess)) {
-		errorMessages.innerText = "<poke>Hey! That's not a valid number!";
+		errorMessages.innerText = "Hey, now you're just messing with me! Please enter a valid number.";
 		clearInput();
-	} else if (usersLastGuess < 1 || usersLastGuess > 100) {
-		errorMessages.innerText = "<poke>Hey! Pick a number between 1 and 100!";
+	} else if (usersLastGuess < minVal || usersLastGuess > maxVal) {
+		errorMessages.innerText = "Really? C'mon, pick a number between " + minVal + " and " + maxVal + ".";
 		clearInput();
 	} else {
 		errorMessages.innerText = '';
+		console.log('Random number is currently ' + randomNumber);
+		if (randomNumber == null) {
+			getRandomNumber();
+		}
 		evaluateGuess();
 	}
 });
@@ -42,71 +70,100 @@ guessButton.addEventListener('click', function () {
 // Disable or Enable certain buttons as the user enters text.
 guessInput.addEventListener('input', function () {
 	if (guessInput.value.length > 0) {
-		enableButton(clearButton);
-		enableButton(resetButton);
+		enableElement(clearButton);
+		enableElement(resetButton);
 	} else {
-		disableButton(clearButton);
+		disableElement(clearButton);
 		if (usersLastGuess == null) {
 			// Also disable RESET, because they haven't yet submitted anything.
-			disableButton(resetButton);
+			disableElement(resetButton);
 		}
 	}
 })
 
 // This function will enable whatever button is passed in as a parameter, and it will update the CSS class appropriately
-function enableButton(buttonToEnable) {
-	console.log('function call to enableButton()');
-	buttonToEnable.disabled = false;
-	buttonToEnable.classList.add('buttonEnabled');
-	buttonToEnable.classList.remove('buttonDisabled');
+function enableElement(elementToEnable) {
+	elementToEnable.disabled = false;
 }
 
 // This function will disable whatever button is passed in as a parameter, and it will update the CSS class appropriately
-function disableButton(buttonToDisable) {
-	console.log('function call to disableButton()');
-	buttonToDisable.disabled = true;
-	buttonToDisable.classList.add('buttonDisabled');
-	buttonToDisable.classList.remove('buttonEnabled');
+function disableElement(elementToDisable) {
+	elementToDisable.disabled = true;
 }
 
 // Reset the application interface to zero state
 function setZeroState() {
-	getRandomNumber();
-	disableButton(clearButton);
-	disableButton(resetButton);
+	disableElement(clearButton);
+	disableElement(resetButton);
+	enableElement(minRangeValue);
+	enableElement(maxRangeValue);
 	usersLastGuess = null;
-	guessMessageTop.innerText = "Welcome!";
-	guessValue.innerText = '';
+	randomNumber = null;
+	minRangeValue.value = '1';
+	maxRangeValue.value = '100';
+	guessMessageTop.innerText = "";
+	guessValue.innerText = '?';
+	guessValue.classList.add('guessValue');
+	guessValue.classList.remove('boom');
 	guessMessageBottom.innerText = '';
 	errorMessages.innerText = '';
 }
 
 // Function to generate a random number and assign it to a global variable
 function getRandomNumber() {
-	randomNumber = Math.floor(Math.random() * 100 + 1);
-	console.log('Random number is currently ' + randomNumber);
+	var min = minRangeValue.value;
+	var max = maxRangeValue.value;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+	console.log('New random number generation.' +
+		'\nMin: ' + min + ', Max: ' + max +
+		'\nRandom Number is now: ' + randomNumber);
 }
 
 // Function to clear the guess input field and set focus to the input element
 function clearInput() {
 	guessInput.value = '';
 	guessInput.focus();
+	disableElement(clearButton);
+	if (usersLastGuess == null) {
+		// Also disable RESET, because they haven't yet submitted anything.
+		disableElement(resetButton);
+	}
 }
 
 // Function to evaluate the users guess compared to the current random number
 function evaluateGuess() {
-	console.log('Evaluate the users guess. If we are here, it is a valid number.');
-
+	console.log('Min: ' + minRangeValue.value + '\n' +
+		'Max: ' + maxRangeValue.value + '\n' +
+		'Random number: ' + randomNumber + '\n' +
+		'User guess: ' + usersLastGuess);
+	// Disable the min and max ranges (only to be re-enabled on reset)
+	disableElement(minRangeValue);
+	disableElement(maxRangeValue);
 	errorMessages.innerText = '';
 	guessMessageTop.innerText = "Your last guess was";
 	guessValue.innerText = usersLastGuess;
-
+	guessValue.classList.add('guessValue');
+	guessValue.classList.remove('boom');
 	if (usersLastGuess > randomNumber) {
 		guessMessageBottom.innerText = "That is too high";
 	} else if (usersLastGuess < randomNumber) {
 		guessMessageBottom.innerText = "That is too low";
 	} else {
-		guessMessageBottom.innerText = "BOOM!";
+		guessMessageTop.innerText = "Lucky guess! Keep going..."
+		guessMessageBottom.innerText = "But now we'll increase the range.";
+		guessValue.innerText = '';
+		guessValue.classList.add('boom');
+		guessValue.classList.remove('guessValue');
+		adjustMinMax();
+		getRandomNumber();
 	}
 	clearInput();
+}
+
+// Function that will subtract 10 from the Min Range and add 10 to the Max Range, which should only happen when the user guesses the random number correctly.
+function adjustMinMax() {
+	minRangeValue.value = parseInt(minRangeValue.value, 10) - 10;
+	maxRangeValue.value = parseInt(maxRangeValue.value, 10) + 10;
 }
